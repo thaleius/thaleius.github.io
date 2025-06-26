f544f618b9726b0719620445fbb74dccf7618e44
thaleius <58767350+thaleius@users.noreply.github.com>
2025-06-26 15:06:45 +0200

    Wrap children in a container div for improved layout structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="container min-h-screen min-w-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
+	{@render children()}
+</div>
